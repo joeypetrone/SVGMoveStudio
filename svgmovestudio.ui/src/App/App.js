039e4dd5293cc7a8126e9e9862300051fd31adf4
Joey Petrone <joey.petrone@gmail.com>
2020-11-28 17:11:12 -0600

    front end App is setup and ready for developement

---
+++ svgmovestudio.ui/src/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>SVG Move Studio</h2>
+      <button className="btn btn-danger">Press Me!</button>
+    </div>
+  );
+}
+
+export default App;
