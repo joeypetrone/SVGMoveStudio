6a635e1ca3f5bf61a1cede13273a512d66b1d4fe
Joey Petrone <joey.petrone@gmail.com>
2020-12-01 20:29:39 -0600

    ObjectType Data ready for use

---
+++ SVGMoveStudio/Data/ObjectTypeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SVGMoveStudio.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace SVGMoveStudio.Data
+{
+    public class ObjectTypeRepository
+    {
+        const string _connectionString = "Server=localhost;Database=SVGMoveStudio;Trusted_Connection=True;";
+
+        public List<ObjectType> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var objectTypes = db.Query<ObjectType>("SELECT * FROM ObjectType");
+
+            return objectTypes.ToList();
+        }
+
+        internal ObjectType GetObjectTypeById(int objectTypeId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var query = @"SELECT *
+                          FROM ObjectType
+                          WHERE ObjectTypeId = @otid";
+
+            var parameters = new { otid = objectTypeId };
+
+            var objectType = db.QueryFirstOrDefault<ObjectType>(query, parameters);
+
+            return objectType;
+        }
+
+        internal void Remove(int objectTypeId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var query = @"DELETE
+                          FROM ObjectType
+                          WHERE ObjectTypeId = @otid";
+
+            db.Execute(query, new {otid = objectTypeId });
+        }
+    }
+}
---
+++ SVGMoveStudio/Models/ObjectType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVGMoveStudio.Models
+{
+    public class ObjectType
+    {
+        public int ObjectTypeId { get; set; }
+        public string ObjectTypeName { get; set; }
+    }
+}
