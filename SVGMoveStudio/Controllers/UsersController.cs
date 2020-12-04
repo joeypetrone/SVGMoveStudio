1631e55fe92b0d747f6f8dd4112f628fbb687247
Joey Petrone <joey.petrone@gmail.com>
2020-12-03 22:36:58 -0600

    All users can be accessed by Http request

---
+++ SVGMoveStudio/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SVGMoveStudio.Data;
+
+namespace SVGMoveStudio.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UserRepository _repo;
+
+        public UsersController()
+        {
+            _repo = new UserRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var allUsers = _repo.GetAll();
+
+            return Ok(allUsers);
+        }
+    }
+}
---
+++ SVGMoveStudio/Data/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SVGMoveStudio.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.SqlClient;
+using Dapper; 
+
+namespace SVGMoveStudio.Data
+{
+    public class UserRepository
+    {
+        const string _connectionString = "Server=localhost;Database=SVGMoveStudio;Trusted_Connection=true;";
+
+        public List<User> GetAll()
+        {
+            var db = new SqlConnection(_connectionString);
+
+            var allUsers = db.Query<User>("SELECT * FROM [User]");
+
+            return allUsers.ToList();
+        }
+    }
+}
