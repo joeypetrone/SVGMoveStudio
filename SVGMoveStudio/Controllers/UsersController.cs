59fe1423af2d990c13dcb380dec2b22057441585
Joey Petrone <joey.petrone@gmail.com>
2020-12-03 23:15:21 -0600

    Single User GET and DELETE Http Requests working

--- SVGMoveStudio/Controllers/UsersController.cs
+++ SVGMoveStudio/Controllers/UsersController.cs
@@ -26,5 +26,31 @@ namespace SVGMoveStudio.Controllers
 
             return Ok(allUsers);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleUser(int id)
+        {
+            var singleUser = _repo.GetUserById(id);
+
+            if (singleUser == null)
+            {
+                return NotFound("User with that id not found in the database.");
+            }
+
+            return Ok(singleUser);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserById(int id)
+        {
+            if (_repo.GetUserById(id) == null)
+            {
+                NotFound("User with that id not found in the database.");
+            }
+
+            _repo.Remove(id);
+
+            return Ok();
+        }
     }
 }
--- SVGMoveStudio/Data/UserRepository.cs
+++ SVGMoveStudio/Data/UserRepository.cs
@@ -21,5 +21,33 @@ namespace SVGMoveStudio.Data
 
             return allUsers.ToList();
         }
+
+        public User GetUserById(int userId)
+        {
+            var db = new SqlConnection(_connectionString);
+
+            var query = @"SELECT *
+                          FROM [User]
+                          WHERE UserId = @uid";
+
+            var parameters = new { uid = userId };
+
+            var singleUser = db.QueryFirstOrDefault<User>(query, parameters);
+
+            return singleUser;
+        }
+
+        public void Remove(int userId)
+        {
+            var db = new SqlConnection(_connectionString);
+
+            var query = @"DELETE
+                          FROM [User]
+                          WHERE UserId = @uid";
+
+            var parameters = new { uid = userId };
+
+            db.Execute(query, parameters);
+        }
     }
 }
