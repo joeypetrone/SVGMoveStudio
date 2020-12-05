6fac3abee1c4675a2edabb69c620da92d1366bd5
Joey Petrone <joey.petrone@gmail.com>
2020-12-05 10:14:46 -0600

    Setup components to test routing

---
+++ svgmovestudio.ui/src/components/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div className="Home">
+        In Home
+      </div>
+    )
+  }
+}
+
+export default Home;
---
+++ svgmovestudio.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return(
+      <div className="MyNavbar">
+        Navbar
+      </div>
+    )
+  }
+}
+
+export default MyNavbar;
