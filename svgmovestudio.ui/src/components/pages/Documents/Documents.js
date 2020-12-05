e19d9ad188d9e7e1313b94c61bff7f29d33b871b
Joey Petrone <joey.petrone@gmail.com>
2020-12-05 11:58:40 -0600

    Routing is working properly

---
+++ svgmovestudio.ui/src/components/pages/Documents/Documents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Documents.scss';
+
+class Documents extends React.Component {
+  render() {
+    return(
+      <div className="Documents">
+        In Documents
+      </div>
+    )
+  }
+}
+
+export default Documents;
--- svgmovestudio.ui/src/components/shared/MyNavbar/MyNavbar.js
+++ svgmovestudio.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
   render() {
+
+
     return(
       <div className="MyNavbar">
         Navbar
+      <Link className="btn btn-danger" to={'/home'}>home</Link>
+      <Link className="btn btn-danger" to={'/documents'}>documents</Link>
       </div>
     )
   }
