e657f89d372bfbb2ca66675dd4a3b008be3fba70
Joey Petrone <joey.petrone@gmail.com>
2021-02-01 00:29:42 -0600

    documents top navbar tabs become active when clicked

--- svgmovestudio.ui/src/components/pages/Documents/Documents.js
+++ svgmovestudio.ui/src/components/pages/Documents/Documents.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Jumbotron, Container, Row, Col } from 'reactstrap';
+import { Jumbotron, Container, Row, Nav, NavItem, NavLink } from 'reactstrap';
 import './Documents.scss';
 
 class Documents extends React.Component {
+  state = {
+    overviewActive: true,
+    refereneceActive: false
+  }
+
+  overviewClickEvent = (e) => {
+    e.preventDefault()
+    this.setState({ overviewActive: true, refereneceActive: false})
+  }
+
+  referenceClickEvent = (e) => {
+    e.preventDefault()
+    this.setState({ overviewActive: false, refereneceActive: true})
+  }
+
   render() {
+    const { overviewActive, refereneceActive } = this.state;
+
     return(
       <div className="Documents">
         <Jumbotron className="m-auto bg-white w-100 py-4">
@@ -11,14 +28,22 @@ class Documents extends React.Component {
           <h1 className="">take a look at our documents</h1>
           <p className="lead">Here you can find information on various parts of the editor</p>
         </Jumbotron>
-        <Container className="documents-body mb-3 rounded">
-          <Row className="mt-3">
-            <Col className="card p-1 m-1" xs="2">
-              By signing up, you gain access to save your SVGs and SVG animations. Signing up is easy and completely free. Sign up now or continue as a guest.
-            </Col>
-            <Col className="card rounded p-1 m-1" xs="9">
-              By signing up, you gain access to save your SVGs and SVG animations. Signing up is easy and completely free. Sign up now or continue as a guest.
-            </Col>
+        <Container className="documents-body mb-3 p-1 rounded">
+          <Nav tabs>
+            <NavItem>
+              <NavLink className="text-dark" href="#" active={overviewActive} onClick={this.overviewClickEvent}>Overview</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="text-dark" href="#" active={refereneceActive} onClick={this.referenceClickEvent}>Reference</NavLink>
+            </NavItem>
+          </Nav>
+          <Row className="m-0">
+            <div className="documents-navbar bg-white p-1 mb-1 mr-1 rounded border">
+
+            </div>
+            <div className="documents-textarea bg-white p-1 mb-1 rounded border">
+
+            </div>
           </Row> 
         </Container>  
       </div>
---
+++ svgmovestudio.ui/src/components/shared/DocumentsTabs/DocumentsOverview/DocumentsOverview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './DocumentsOverview'
+
+class DocumentsOverview extends React.Component {
+  render() {
+    return (
+      <div className="DocumentsOverview card">
+        Documents Overview
+      </div>
+    )
+  }
+}
+
+export default DocumentsOverview;
