14f514f6f85ad299db25224c3b8f33fc1beb7914
Joey Petrone <joey.petrone@gmail.com>
2021-03-21 00:38:07 -0500

    When save button is pressed, updated element attributes are set in the database

--- SVGMoveStudio/Data/ElementRepository.cs
+++ SVGMoveStudio/Data/ElementRepository.cs
@@ -171,7 +171,72 @@ namespace SVGMoveStudio.Data
 
         internal void Update(Element element)
         {
-            throw new NotImplementedException();
+            var db = new SqlConnection(_connectionString);
+
+            var query = @"UPDATE [dbo].[Element]
+                             SET [SVGId] = @svgid
+                                ,[ElementTypeId] = @elementtypeid
+                                ,[ElementName] = @elementname
+                                ,[ArrangeNumber] = @arrangenumber
+                                ,[Fill] = @fill
+                                ,[FillOpacity] = @fillopacity
+                                ,[Opacity] = @opacity
+                                ,[Stroke] = @stroke
+                                ,[StrokeWidth] = @strokewidth
+                                ,[StrokeOpacity] = @strokeopacity
+                                ,[Width] = @width
+                                ,[Height] = @height
+                                ,[X_CoordinateStart] = @x_coordinatestart
+                                ,[Y_CoordinateStart] = @y_coordinatestart
+                                ,[X_CoordinateEnd] = @x_coordinateend
+                                ,[Y_CoordinateEnd] = @y_coordinateend
+                                ,[X_Radius] = @x_radius
+                                ,[Y_Radius] = @y_radius
+                                ,[Points] = @points
+                                ,[PathShape] = @pathshape
+                                ,[PathLength] = @pathlength
+                                ,[X_Translate] = @x_translate
+                                ,[Y_Translate] = @y_translate
+                                ,[Rotate] = @rotate
+                                ,[Scale] = @scale
+                                ,[X_Skew] = @x_skew
+                                ,[Y_Skew] = @y_skew
+                                ,[isDefault] = @isdefault
+                           WHERE [Elementid] = @elementid";
+            var parameters = new
+            {
+                elementid = element.ElementId,
+                svgid = element.SVGId,
+                elementtypeid = element.ElementTypeId,
+                elementname = element.ElementName,
+                arrangenumber = element.ArrangeNumber,
+                fill = element.Fill,
+                fillopacity = element.FillOpacity,
+                opacity = element.Opacity,
+                stroke = element.Stroke,
+                strokewidth = element.StrokeWidth,
+                strokeopacity = element.StrokeOpacity,
+                width = element.Width,
+                height = element.Height,
+                x_coordinatestart = element.X_CoordinateStart,
+                y_coordinatestart = element.Y_CoordinateStart,
+                x_coordinateend = element.X_CoordinateEnd,
+                y_coordinateend = element.Y_CoordinateEnd,
+                x_radius = element.X_Radius,
+                y_radius = element.Y_Radius,
+                points = element.Points,
+                pathshape = element.PathShape,
+                pathlength = element.PathLength,
+                x_translate = element.X_Translate,
+                y_translate = element.Y_Translate,
+                rotate = element.Rotate,
+                scale = element.Scale,
+                x_skew = element.X_Skew,
+                y_skew = element.Y_Skew,
+                isdefault = element.isDefault
+            };
+
+            db.Execute(query, parameters);
         }
 
         public void Remove(int elementId)
